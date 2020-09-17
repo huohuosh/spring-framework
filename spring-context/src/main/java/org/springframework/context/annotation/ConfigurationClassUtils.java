879083f396fbf5c5a39d997d1a1387d0d1f8ece7
chendongqi <chendongqi@huaxiafinance.com>
2020-09-17 16:59:05 +0800

    ConfigurationClassUtils

--- spring-context/src/main/java/org/springframework/context/annotation/ConfigurationClassUtils.java
+++ spring-context/src/main/java/org/springframework/context/annotation/ConfigurationClassUtils.java
@@ -46,14 +46,47 @@ import org.springframework.stereotype.Component;
  * @since 3.1
  */
 abstract class ConfigurationClassUtils {
-
+	/**
+	 * Full模式
+	 * 优点：
+	 * - 可以支持通过常规Java调用相同类的@Bean方法而保证是容器内的Bean，这有效规避了在“Lite模式”下操作时难以跟踪的细微错误。
+	 * 缺点：
+	 * - 运行时会给该类生成一个CGLIB子类放进容器，有一定的性能、时间开销（这个开销在Spring Boot这种拥有大量配置类的情况下是不容忽视的，
+	 *   这也是为何Spring 5.2新增了proxyBeanMethods属性的最直接原因）
+	 * - 正因为被代理了，所以@Bean方法 不可以是private、不可以是final
+	 *
+	 * 该模式下，配置类会被 CGLIB 增强(生成代理对象)，放进IoC容器内的是代理
+	 * 该模式下，对于内部类是没有限制的：可以是Full模式或者Lite模式
+	 * 该模式下，配置类内部可以通过方法调用来处理依赖，并且能够保证是同一个实例，都指向IoC内的那个单例
+	 * 该模式下，@Bean方法不能被private/final等进行修饰
+	 */
 	private static final String CONFIGURATION_CLASS_FULL = "full";
-
+	/**
+	 * Lite模式
+	 * 和 Full模式的 @Configuration不同，Lite模式的 @Bean方法不能声明 Bean 之间的依赖关系。
+	 * 因此，这样的@Bean方法不应该调用其他@Bean方法。
+	 * 每个这样的方法实际上只是一个特定Bean引用的工厂方法(factory-method)，没有任何特殊的运行时语义。
+	 * 优点：
+	 * - 运行时不再需要给对应类生成CGLIB子类，提高了运行性能，降低了启动时间
+	 * - 可以该配置类当作一个普通类使用喽：也就是说 @Bean方法 可以是 private、可以是 final
+	 * 缺点：
+	 * - 不能声明@Bean之间的依赖，也就是说不能通过方法调用来依赖其它 Bean
+	 * （其实这个缺点还好，很容易用其它方式“弥补”，比如：把依赖Bean放进方法入参里即可）
+	 *
+	 * 该模式下，配置类本身不会被 CGLIB 增强，放进 IoC 容器内的就是本尊
+	 * 该模式下，对于内部类是没有限制的：可以是Full模式或者Lite模式
+	 * 该模式下，配置类内部不能通过方法调用来处理依赖，否则每次生成的都是一个新实例而并非IoC容器内的单例
+	 * 该模式下，配置类就是一普通类，所以 @Bean方法可以使用private/final等进行修饰（static也可以）
+	 */
 	private static final String CONFIGURATION_CLASS_LITE = "lite";
-
+	/**
+	 * 属性名，属性存储模式类型
+	 */
 	private static final String CONFIGURATION_CLASS_ATTRIBUTE =
 			Conventions.getQualifiedAttributeName(ConfigurationClassPostProcessor.class, "configurationClass");
-
+	/**
+	 * 属性名，属性存储 Configuration 顺序
+	 */
 	private static final String ORDER_ATTRIBUTE =
 			Conventions.getQualifiedAttributeName(ConfigurationClassPostProcessor.class, "order");
 
@@ -134,6 +167,7 @@ abstract class ConfigurationClassUtils {
 	/**
 	 * Check the given metadata for a configuration class candidate
 	 * (or nested component class declared within a configuration/component class).
+	 * 检测是否是配置类
 	 * @param metadata the metadata of the annotated class
 	 * @return {@code true} if the given class is to be registered as a
 	 * reflection-detected bean definition; {@code false} otherwise
@@ -145,6 +179,7 @@ abstract class ConfigurationClassUtils {
 	/**
 	 * Check the given metadata for a full configuration class candidate
 	 * (i.e. a class annotated with {@code @Configuration}).
+	 * 配置类是否为 full 模式 （类上有 @Configuration 注解，5.2之后且 @Configuration(proxyBeanMethods = true)）
 	 * @param metadata the metadata of the annotated class
 	 * @return {@code true} if the given class is to be processed as a full
 	 * configuration class, including cross-method call interception
@@ -157,6 +192,12 @@ abstract class ConfigurationClassUtils {
 	 * Check the given metadata for a lite configuration class candidate
 	 * (e.g. a class annotated with {@code @Component} or just having
 	 * {@code @Import} declarations or {@code @Bean methods}).
+	 *
+	 * 配置类是否为 lite 模式（非接口;
+	 * 在 {@link #candidateIndicators} 中
+	 * 或有被 @Bean 标注的方法
+	 * 或 5.2之 @Configuration(proxyBeanMethods = false)）
+	 *
 	 * @param metadata the metadata of the annotated class
 	 * @return {@code true} if the given class is to be processed as a lite
 	 * configuration class, just registering it and scanning it for {@code @Bean} methods
@@ -189,6 +230,7 @@ abstract class ConfigurationClassUtils {
 	/**
 	 * Determine whether the given bean definition indicates a full {@code @Configuration}
 	 * class, through checking {@link #checkConfigurationClassCandidate}'s metadata marker.
+	 * 判断是否是 full 模式配置类
 	 */
 	public static boolean isFullConfigurationClass(BeanDefinition beanDef) {
 		return CONFIGURATION_CLASS_FULL.equals(beanDef.getAttribute(CONFIGURATION_CLASS_ATTRIBUTE));
@@ -197,6 +239,7 @@ abstract class ConfigurationClassUtils {
 	/**
 	 * Determine whether the given bean definition indicates a lite {@code @Configuration}
 	 * class, through checking {@link #checkConfigurationClassCandidate}'s metadata marker.
+	 * 判断是否是 lite 模式配置类
 	 */
 	public static boolean isLiteConfigurationClass(BeanDefinition beanDef) {
 		return CONFIGURATION_CLASS_LITE.equals(beanDef.getAttribute(CONFIGURATION_CLASS_ATTRIBUTE));
@@ -204,6 +247,7 @@ abstract class ConfigurationClassUtils {
 
 	/**
 	 * Determine the order for the given configuration class metadata.
+	 * 配置类顺序
 	 * @param metadata the metadata of the annotated class
 	 * @return the {@code @Order} annotation value on the configuration class,
 	 * or {@code Ordered.LOWEST_PRECEDENCE} if none declared
@@ -211,6 +255,7 @@ abstract class ConfigurationClassUtils {
 	 */
 	@Nullable
 	public static Integer getOrder(AnnotationMetadata metadata) {
+		// 获取 @Order 属性
 		Map<String, Object> orderAttributes = metadata.getAnnotationAttributes(Order.class.getName());
 		return (orderAttributes != null ? ((Integer) orderAttributes.get(AnnotationUtils.VALUE)) : null);
 	}
@@ -218,6 +263,7 @@ abstract class ConfigurationClassUtils {
 	/**
 	 * Determine the order for the given configuration class bean definition,
 	 * as set by {@link #checkConfigurationClassCandidate}.
+	 * 得到 BeanDefinition 中 order 属性的值
 	 * @param beanDef the bean definition to check
 	 * @return the {@link Order @Order} annotation value on the configuration class,
 	 * or {@link Ordered#LOWEST_PRECEDENCE} if none declared
